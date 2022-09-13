487f0ce22c935361226993e715e40944d1c2af67
Sahsa <a.v.pavlov09@gmail.com>
2022-09-13 22:55:53 +0300

    Example 005 If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+
+if(username == "Серёжка")
+{
+    Console.WriteLine("Приветствую тебя, машина!!!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
