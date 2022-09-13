25513cbe3ea3eb2f77e2478c4f9109ba8d49bb49
Sahsa <a.v.pavlov09@gmail.com>
2022-09-13 22:41:01 +0300

    Random Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);//случайное число от 1 до 10. Максимальное число будеть 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);//1 2 3 4....9
+Console.WriteLine(numberB);
 Console.Write(numberA + numberB);
